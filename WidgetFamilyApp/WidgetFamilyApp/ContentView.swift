7ebc5658a499d7e5f0f4c30f24d4aa545536d534
yeahg-dev <yeahg.dev@gmail.com>
2022-03-15 21:22:08 +0900

    feat: WidgetFamilyApp 프로젝트 생성, WidgetFamily 타겟 추가

---
+++ WidgetFamilyApp/WidgetFamilyApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WidgetFamilyApp
+//
+//  Created by 1 on 2022/03/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WidgetFamilyApp/WidgetFamilyApp/WidgetFamilyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetFamilyAppApp.swift
+//  WidgetFamilyApp
+//
+//  Created by 1 on 2022/03/15.
+//
+
+import SwiftUI
+
+@main
+struct WidgetFamilyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
